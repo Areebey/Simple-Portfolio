d98ef3a6a979f5004dba685e6e8ea1edf50a7435
Areebey <areebrafiq125@gmail.com>
2023-12-16 21:24:49 +0500

    Submit button updated

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 
-let btn=document.getElementById("cv-btn");
-btn.addEventListener("click",function(){
+let cvBtn=document.getElementById("cv-btn");
+let submitBtn=document.getElementById("submit");
+
+cvBtn.addEventListener("click",function(){
     alert("Resume Download Sucessfully")
 })
+
+
+submitBtn.addEventListener("click",function(){
+    let email=document.getElementById("email");
+    let num=document.getElementById("number")
+    if(email.value==""&& num.value==""){
+        alert("Fill the Details Please!");
+    }
+    else{
+        alert("Message Submitted!");
+    }
+})
+
